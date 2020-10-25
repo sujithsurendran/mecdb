8cafff33353d936b1798ca3038df86788cb1dd10
sujith <sujith@sujith>
2020-10-25 08:56:38 +0530

    initial update-001

---
+++ mecdb/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from django.test import RequestFactory
+
+from mecdb.users.models import User
+from mecdb.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
+
+
+@pytest.fixture
+def request_factory() -> RequestFactory:
+    return RequestFactory()
